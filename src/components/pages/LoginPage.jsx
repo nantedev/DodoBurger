93175980078d132b1c00b512b925a98685024732
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-08-07 21:52:38 +0400

    ✨ F01: LOGIN > Add title and form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,6 +1,39 @@
+import { useState } from "react"
+
 export default function LoginPage() {
-  return (
-    <div>login Page</div>
-  )
+    
+    //state
+    const [inputValue, setInputValue] = useState("")
+
+      //comportements
+    const handleSubmit = (event) => {
+      event.preventDefault() 
+      alert(`Bienvenue ${inputValue}`)
+      setInputValue("")
+     }
+
+    const handleChange = (event) => {
+      setInputValue(event.target.value)
+     }
+      //affichage (render)
+
+
+    return (
+      <div>
+        <h1>Bienvenue chez nous</h1>
+        <br />
+        <h2>Connectez-vous</h2>
+        <form action="submit" onSubmit={handleSubmit}>
+          <input 
+            value={inputValue} 
+            onChange={handleChange} 
+            type="text" 
+            placeholder="Entrez votre prénom..." 
+            required
+            />
+          <button>Accédez à votre espace</button>
+        </form>
+      </div>
+    )
 }
 
