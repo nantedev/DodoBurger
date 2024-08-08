deb4ce587766ab0e8e8f04ef60218199ccdc54eb
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-08-08 22:02:48 +0400

    🎨 LoginPage: refactoring

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,39 +1,8 @@
-import { useState } from "react"
+import LoginForm from "./LoginForm"
 
 export default function LoginPage() {
-    
-    //state
-    const [inputValue, setInputValue] = useState("")
 
-      //comportements
-    const handleSubmit = (event) => {
-      event.preventDefault() 
-      alert(`Bienvenue ${inputValue}`)
-      setInputValue("")
-     }
-
-    const handleChange = (event) => {
-      setInputValue(event.target.value)
-     }
-      //affichage (render)
-
-
-    return (
-      <div>
-        <h1>Bienvenue chez nous !</h1>
-        <br />
-        <h2>Connectez-vous</h2>
-        <form action="submit" onSubmit={handleSubmit}>
-          <input 
-            value={inputValue} 
-            onChange={handleChange} 
-            type="text" 
-            placeholder="Entrez votre prénom..." 
-            required
-            />
-          <button>Accédez à votre espace</button>
-        </form>
-      </div>
-    )
+      // affichage (render)
+    return <LoginForm />
 }
 
