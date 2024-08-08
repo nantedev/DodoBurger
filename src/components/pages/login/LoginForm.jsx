c0a26870e01b0941b14107ba49c2f4badfed48d6
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-08-08 22:19:34 +0400

    ✨ F02: LOGIN > add redirection from/to OrderPage

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Link } from "react-router-dom"
 
 export default function LoginForm() {
     // state
@@ -30,6 +31,7 @@ export default function LoginForm() {
             required
             />
           <button>Accédez à votre espace</button>
+          <Link to="/order">Vers order page</Link>
         </form>
     </div>
   )
--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function OrderPage () {
     return (
-        <div>OrderPage</div>
+        <div>
+            <h1>Order Page</h1>
+            <Link to="/"><button>Deconnexion</button></Link>    
+        </div>
     )
 }
