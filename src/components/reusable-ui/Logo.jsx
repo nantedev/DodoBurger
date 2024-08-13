a6f44c354ebd2c48c9a51c3a8546c8fd087d5226
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-08-13 09:55:58 +0400

    💄 F03: Logo, project name fix

--- src/components/reusable-ui/Logo.jsx
+++ src/components/reusable-ui/Logo.jsx
@@ -3,10 +3,37 @@ import styled from 'styled-components';
 
 export default function Logo() {
   return (
-    <LogoStyled>Crazee Burger</LogoStyled>
+    <LogoStyled>
+      <h1>Dodo</h1> 
+      <img src="/images/logo-burger.png" alt="" />
+      <h1>Burger</h1>
+      </LogoStyled>
   )
 }
 
 const LogoStyled = styled.div`
-  background: yellow;
+    display: flex;
+    align-items: center;
+    zoom: 2.5;
+    // transform: scale(2.5);
+
+    h1 {
+      display: inline;
+      text-align: center;
+      color: #ffa01b;
+      font-size: 36px;
+      Line-height: 1em;
+      font-weight: 700;
+      text-transform: uppercase;
+      Letter-spacing: 1.5px;
+      font-family: "Amatic SC", cursive;
+    }
+
+    img {
+      object-fit: contain;
+      object-position: center;
+      height: 60px;
+      width: 80px;
+      margin: 0 5px;
+    }
 `;
