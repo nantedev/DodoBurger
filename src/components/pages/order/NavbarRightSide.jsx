ae88119b995688783108ceab80276c379afd0028
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-08-17 14:26:02 +0400

    ✨ F04: Order > Layout > navbar > create profile

--- src/components/pages/order/NavbarRightSide.jsx
+++ src/components/pages/order/NavbarRightSide.jsx
@@ -1,17 +1,27 @@
 import { Link } from "react-router-dom"
 import styled from "styled-components";
+import Profile from "./Profile";
 
 export default function NavbarRightSide({ username }) {
   return (
     <NavbarRightSideStyled>
-    <h1>Bonjour { username }</h1>
-    <Link to="/">
-        <button>Deconnexion</button>
-    </Link>
+        {/* <div className="admin-button">Admin Button</div> */}
+        <Profile />
     </NavbarRightSideStyled>
   )
 }
 
 const NavbarRightSideStyled = styled.div`
     background-color: purple;
+    display: flex;
+    align-items: center;
+    padding-right: 50px;
+
+    /* .admin-button {
+        background-color: lightblue;
+    } */
+
+    .profile {
+        background-color: yellow;
+    }
 `
