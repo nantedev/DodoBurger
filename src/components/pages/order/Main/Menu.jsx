2f488e808547e3771a04e71975dbc75429edfd0b
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-08-21 17:29:48 +0400

    💄 F05: order > Main > Product > image layout

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -28,6 +28,4 @@ const StyledMenu  = styled.div`
     padding: 50px 50px 150px;
     justify-items: center;
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-
-
 `;
--- src/components/pages/order/Main/Product.jsx
+++ src/components/pages/order/Main/Product.jsx
@@ -7,8 +7,8 @@ export default function Product({title, imageSource, price}) {
         <img src={imageSource} alt={title} />
     </div>
     <div className="info-text"> 
+    <div className="title">{title}</div>
         <div className="description">
-            <div className="title">{title}</div>
             <div className="price">{price}</div>
             <button className="add-button">Ajouter</button>
         </div>
@@ -21,18 +21,25 @@ const ProduitStyled = styled.div`
         background: red;
         width: 240px;
         height: 330px;
+        display: grid;
+        grid-template-rows: 65% 1fr;
+        box-sizing: border-box;
+        padding: 0 20px 10px;
 
         .image {
         border: 1px solid pink;
-        width: 100px;
+        width: 100%;
         height: auto;
+        margin-top: 30px;
+
         img {
             height: 100%;
             width: 100%;
-            object-fit: cover;
+            object-fit: contain;
         }
         }
+        
         .description {
-            border: 1px solid pink;
+            border: 1px solid blue;
         }
 `;
