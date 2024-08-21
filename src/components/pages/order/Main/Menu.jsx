f612aef3b67ebe975e7056fc29636d685c0e4c5c
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-08-21 10:19:51 +0400

    🍱 F05: Add assets

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 export default function Menu() {
-  return (
+  
+  const [Menu, setMenu] = useState(second)
+
+    return (
     <StyledMenu>Menu</StyledMenu>
   )
 }
