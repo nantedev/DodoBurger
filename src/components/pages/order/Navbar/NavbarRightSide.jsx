bb773db89269a694b38212144707c1ec42894f3c
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-08-31 13:55:16 +0400

    ♻️ F06: Navbar > RightSide > extract ToastAdmin

--- src/components/pages/order/Navbar/NavbarRightSide.jsx
+++ src/components/pages/order/Navbar/NavbarRightSide.jsx
@@ -1,10 +1,9 @@
 import styled from "styled-components"
 import Profile from "./Profile"
 import ToggleButton from "../../../reusable-ui/ToggleButton"
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import { theme } from "../../../../theme"
+import { toast } from 'react-toastify'
 import { useState } from "react"
+import ToastAdmin from "./ToastAdmin"
 
 export default function NavbarRightSide({ username }) {
   const [isModeAdmin, setIsModeAdmin] = useState(false)
@@ -33,7 +32,7 @@ export default function NavbarRightSide({ username }) {
         onToggle={displayToastNotification}
         />
         <Profile username={ username }/>
-        <ToastContainer className="toaster" bodyClassName="body-toast"/>
+        <ToastAdmin />
     </NavbarRightSideStyled>
   )
 }
@@ -46,21 +45,4 @@ const NavbarRightSideStyled = styled.div`
     .toggle-button {
       border: 1px solid red;
     }
-    .toaster {
-    max-width: 300px;
-  }
-
-  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
-    background: ${theme.colors.background_dark};
-  }
-
-  .body-toast {
-    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
-      margin-right: 20px;
-      margin-left: 5px;
-    }
-    div {
-      line-height: 1.3em;
-    }
-  }
 `
