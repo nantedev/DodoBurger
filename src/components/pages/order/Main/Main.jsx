e4bc48db4b732bdc90ebe7df654c22f7eace418b
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-09-02 09:55:55 +0400

    💄 F07: Main > add layout with Basket

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -6,13 +6,18 @@ export default function Main() {
   return (
     <MainStyled>
       {/* <div className="basket">Basket</div> */}
+      <div className="menu-and-admin">
       <Menu />
+        <div className="admin">Admin</div>
+      </div>
     </MainStyled>
   )
 }
 
 const MainStyled  = styled.main`
+    border: 3px solid green; 
     background: ${theme.colors.background_white};
+    height: calc(95vh - 10vh);
     flex: 1;
     border-bottom-left-radius: ${theme.borderRadius.extraRound};
     border-bottom-right-radius: ${theme.borderRadius.extraRound};
@@ -20,10 +25,26 @@ const MainStyled  = styled.main`
 
     display: grid;
     grid-template-columns: 1fr;
-    
-    overflow-y: scroll;
 
     /* .basket {
       background: pink;
     } */
+
+    .menu-and-admin {
+      position: relative;
+      overflow-y: hidden;
+      display: grid;
+      border-bottom-left-radius: ${theme.borderRadius.extraRound};
+      border-bottom-right-radius: ${theme.borderRadius.extraRound};
+      
+      .admin{
+      background: red;
+      height: 250px;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      right: 0;
+    }
+    }
+
 `
