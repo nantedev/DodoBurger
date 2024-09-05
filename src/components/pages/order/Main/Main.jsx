3ab3f54b3bcd07f4d4c34fb579735a5d22eedca8
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-09-05 17:53:08 +0400

    💄 F07: theme > add shadows following design system

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -28,7 +28,7 @@ const MainStyled  = styled.main`
     flex: 1;
     border-bottom-left-radius: ${theme.borderRadius.extraRound};
     border-bottom-right-radius: ${theme.borderRadius.extraRound};
-    box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset ;
+    box-shadow: ${theme.shadows.strong};
 
     display: grid;
     grid-template-columns: 1fr;
