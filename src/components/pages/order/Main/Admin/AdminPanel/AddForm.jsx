b7e85bd681573e480e5dcf820227a91943c729fd
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-09-06 16:35:24 +0400

    🚧 F08: Admin > Panel > add layout inputs

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -2,10 +2,43 @@ import styled from 'styled-components';
 
 export default function AddForm() {
   return (
-    <NewComponentStyled>AddForm</NewComponentStyled>
+    <AddFormStyled>
+      <div className="image-preview">Image Preview</div>
+      <div className="input-fields">
+        <input type="text" placeholder="Name"/>
+        <input type="text" placeholder="Image URL"/>
+        <input type="text" placeholder="Prix"/>
+      </div>
+      <div className="submit-buton">Submit button</div>
+    </AddFormStyled>
   );
 }
 
-const NewComponentStyled = styled.div`
+const AddFormStyled = styled.div`
   border: 1px solid red;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: repeat(4, 1fr);
+  grid-template-areas: 
+    "image-preview input-fields"
+    "image-preview input-fields"
+    "image-preview input-fields"
+    ".             submit-buton";
+
+  height: 100%;
+  width: 70%;
+
+  .image-preview {
+    background: red;
+    grid-area: image-preview ;
+  }
+  .input-fields{
+    background: blue;
+    grid-area: input-fields ;
+    display: grid;
+  }
+  .submit-buton{
+    background: green;
+    grid-area: submit-buton ;
+  }
 `; 
