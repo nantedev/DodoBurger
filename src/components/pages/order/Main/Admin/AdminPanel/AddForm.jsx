3b6ddcf77b93e3d7a011be3a5dd758feadec7338
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-09-09 11:10:40 +0400

    💄  F08 : AddForm > Add style on message when submit

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -87,7 +87,8 @@ export default function AddForm() {
         </div>
         {isSubmitted &&  (
           <div className='submit-message'>
-          <FiCheck /> <span>Ajouté avec succès !</span>
+          <FiCheck className='icon'/> 
+          <span className="message">Ajouté avec succès !</span>
         </div>)}
       </div>
     </AddFormStyled>
@@ -154,7 +155,26 @@ const AddFormStyled = styled.form`
     }
 
     .submit-message{
-      border: 2px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 5px;
+
+      .icon {
+        color: ${theme.colors.success};
+        margin-left: 10px;
+        width: 1em;
+        height: 1em;
+        border: 1px solid ${theme.colors.success};
+        border-radius: 50%;
+        vertical-align: middle;
+      }
+      
+      .message {
+        margin-left: 5px;
+        font-size: ${theme.fonts.size.SM};
+        color: ${theme.colors.success};
+      }
     }
   }
 `; 
