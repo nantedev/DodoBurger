0b4281054d11f1d7fcb09ba43f40ebfa7ae0e880
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-09-09 14:58:47 +0400

    ♻️  F08 : AddForm > add inputTextsConfig

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -1,14 +1,14 @@
 import { useContext, useState } from 'react';
 import styled from 'styled-components';
 import OrderContext from '../../../../../../context/OrderContext';
-import { FiCheck } from 'react-icons/fi';
-import {theme} from '../../../../../../theme'
 import { FaHamburger } from 'react-icons/fa';
 import { BsFillCameraFill } from 'react-icons/bs';
 import { MdOutlineEuro } from 'react-icons/md';
 import TextInput from '../../../../../reusable-ui/TextInput';
 import Button from '../../../../../reusable-ui/Button';
 import ImagePreview from './ImagePreview';
+import SubmitMessage from './SubmitMessage';
+import { getInputTextsConfig } from './inputTextsConfig';
 
 export const EMPTY_PRODUCT = {
   id: "", 
@@ -20,15 +20,13 @@ export const EMPTY_PRODUCT = {
 export default function AddForm() {
 
   //State
-
   const  { handleAddProduct, newProduct, setNewProduct } = useContext(OrderContext)
- 
   const [isSubmitted, setIsSubmitted] = useState(false)
 
   //Comportement
   const handleChange = (event) => {
     const {name, value} = event.target
-      setNewProduct({...newProduct, [name]: value })
+    setNewProduct({...newProduct, [name]: value })
   }
 
   const handleSubmit = (event) => {
@@ -44,49 +42,26 @@ export default function AddForm() {
     setTimeout(() => {
     setIsSubmitted(false)}, 2000)
    }
-  //Affichage
 
+   const inputTexts = getInputTextsConfig(newProduct)
+  
+   //Affichage
   return (
     <AddFormStyled onSubmit={handleSubmit}>
        <ImagePreview imageSource={newProduct.imageSource} title={newProduct.title}/>
       <div className="input-fields">
-        <TextInput 
-        name="title" 
-        value={newProduct.title} 
-        type="text" 
-        placeholder="Nom du produit (ex: Super Dodo Burger)" 
-        onChange={handleChange}
-        Icon={<FaHamburger />}
-        version="minimalist"
-        />
-        <TextInput 
-        name="imageSource" 
-        value={newProduct.imageSource} 
-        type="text" 
-        placeholder="Lien URL d'une image (ex: https://la-photo-de-mon-produit.png)"  
-        onChange={handleChange}
-        Icon={<BsFillCameraFill />}
-        version="minimalist"
-        />
-        <TextInput 
-        name="price" 
-        value={newProduct.price ? newProduct.price : ""} 
-        type="text" 
-        placeholder="Prix"  
-        onChange={handleChange}
-        Icon={<MdOutlineEuro />}
-        version="minimalist"
-        />
+             {inputTexts.map((input) => (
+              <TextInput 
+                {...input}
+                 onChange={handleChange}
+                 Icon={input.Icon}
+                 version="minimalist"
+              />
+             ))} 
       </div>
       <div className="submit">
-        <div className='submit-button'>
-          <Button label="Ajouter un nouveau produit au menu" version="success"/>
-        </div>
-        {isSubmitted &&  (
-          <div className='submit-message'>
-          <FiCheck className='icon'/> 
-          <span className="message">Ajouté avec succès !</span>
-        </div>)}
+        <Button className="submit-button" label="Ajouter un nouveau produit au menu" version="success"/>
+        {isSubmitted &&  (<SubmitMessage />)}
       </div>
     </AddFormStyled>
   )
@@ -107,7 +82,6 @@ const AddFormStyled = styled.form`
   grid-row-gap: 8px;
 
 
-
   .input-fields{
     grid-area: input-fields ;
     display: grid;
@@ -115,36 +89,12 @@ const AddFormStyled = styled.form`
   }
 
   .submit{
-    /* background: green; */
     grid-area: submit;
     display: flex;
     align-items: center;
 
-    .submit-button{
+    .submit-button {
       width: 50%;
-    }
-
-    .submit-message{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-left: 5px;
-
-      .icon {
-        color: ${theme.colors.success};
-        margin-left: 10px;
-        width: 1em;
-        height: 1em;
-        border: 1px solid ${theme.colors.success};
-        border-radius: 50%;
-        vertical-align: middle;
-      }
-      
-      .message {
-        margin-left: 5px;
-        font-size: ${theme.fonts.size.SM};
-        color: ${theme.colors.success};
-      }
     }
   }
 `; 
