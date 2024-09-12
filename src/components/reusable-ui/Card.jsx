54b57b1b1184b722c6f77d028043c8e69e0122cf
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-09-12 15:03:26 +0400

    💄 F09: Card > add isSelectedStyle variant

--- src/components/reusable-ui/Card.jsx
+++ src/components/reusable-ui/Card.jsx
@@ -14,8 +14,8 @@ export default function Card({
   isSelected 
   }) {
   return (
-    <CardStyled onClick={onClick} isHoverable={isHoverable}>
-        <div className="card" style={isSelected ? {background:"orange"} : {}} >
+    <CardStyled onClick={onClick} isHoverable={isHoverable} isSelected={isSelected}>
+        <div className="card" >
           {hasDeleteButton && (
             <button
             className="delete-button"
@@ -41,9 +41,8 @@ export default function Card({
 }
 
 const CardStyled = styled.div`
-  ${(props) => props.isHoverable && hoverableStyle}
+  ${({isHoverable}) => isHoverable && hoverableStyle}
   border-radius: ${theme.borderRadius.extraRound};
-  border: 1px solid red;
   height: 330px; //fix extra card height
 
       .card {
@@ -150,8 +149,10 @@ const CardStyled = styled.div`
         }
       }
     }
+    ${({ isHoverable, isSelected }) => isHoverable && isSelected && selectedStyle}
   }   
 `;
+
 
 const hoverableStyle = css`
   .card:hover {
@@ -159,5 +160,63 @@ const hoverableStyle = css`
     transition: ease-out 0.4s;
     box-shadow: ${theme.shadows.orangeHighlight};
     cursor: pointer;
+  }
+`
+
+
+const selectedStyle = css`
+  background: ${theme.colors.primary};
+  .primary-button {
+    color: ${theme.colors.primary};
+    background-color: ${theme.colors.white};
+    border: 1px solid ${theme.colors.white};
+    transition: all 200ms ease-out;
+    :hover {
+      color: ${theme.colors.white};
+      background-color: ${theme.colors.primary};
+      border: 1px solid ${theme.colors.white};
+      transition: all 200ms ease-out;
+    }
+    :active {
+      background-color: ${theme.colors.white};
+      color: ${theme.colors.primary};
+    }
+
+    &.is-disabled {
+      opacity: 50%;
+      cursor: not-allowed;
+      z-index: 2;
+    }
+
+    &.with-focus {
+      border: 1px solid white;
+      background-color: ${theme.colors.white};
+      color: ${theme.colors.primary};
+      :hover {
+        color: ${theme.colors.white};
+        background-color: ${theme.colors.primary};
+        border: 1px solid ${theme.colors.white};
+      }
+      :active {
+        background-color: ${theme.colors.white};
+        color: ${theme.colors.primary};
+      }
+    }
+  }
+
+  .delete-button {
+    color: ${theme.colors.white};
+
+    :active {
+      color: ${theme.colors.white};
+    }
+  }
+
+  .text-info {
+    .description {
+      .left-description {
+        color: ${theme.colors.white};
+      }
+    }
   }
 `
