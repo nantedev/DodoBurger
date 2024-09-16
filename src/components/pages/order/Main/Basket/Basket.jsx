b6cd568332b0c035fba222629fb6408eee4543d4
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-09-16 23:13:32 +0400

    ♻️ F11: Basket > add BasketCard

--- src/components/pages/order/Main/Basket/Basket.jsx
+++ src/components/pages/order/Main/Basket/Basket.jsx
@@ -6,6 +6,7 @@ import EmptyBasket from './EmptyBasket'
 import { useContext } from 'react'
 import OrderContext from '../../../../../context/OrderContext'
 import BasketProducts from "./BasketProducts"
+import { theme } from '../../../../../theme'
 
 export default function Basket() {
   const {basket} = useContext(OrderContext)
@@ -15,13 +16,28 @@ export default function Basket() {
   return (
     <BasketStyled>
         <Total amountToPay={formatPrice(0)} />
-        {isBasketEmpty ?  <EmptyBasket /> : <BasketProducts />}
+        {isBasketEmpty ?  <EmptyBasket /> : <BasketProducts basket={basket}/>}
         <Footer />
     </BasketStyled>
   );
 }
 
 const BasketStyled = styled.div`
+  background: ${theme.colors.background_white};
+  box-shadow: ${theme.shadows.basket};
   display: flex;
   flex-direction: column;
+  border-bottom-left-radius: ${theme.borderRadius.extraRound};
+  height: 85vh;
+
+  .head {
+    position: sticky;
+    top: 0;
+  }
+
+  .footer {
+    border-bottom-left-radius: ${theme.borderRadius.extraRound};
+    position: sticky;
+    bottom: 0;
+  }
 `
--- src/components/pages/order/Main/Basket/BasketProducts.jsx
+++ src/components/pages/order/Main/Basket/BasketProducts.jsx
@@ -1,10 +1,34 @@
 import styled from 'styled-components';
+import BasketCard from './BasketCard';
 
-export default function BasketProducts() {
+export default function BasketProducts({basket}) {
   return (
-    <BasketProductsStyled>Content</BasketProductsStyled>
+    <BasketProductsStyled>{basket.map((basketProduct) => (
+        <div className='basket-card'>
+          <BasketCard {...basketProduct} />
+        </div>
+        ))}
+    </BasketProductsStyled>
   );
 }
 
 const BasketProductsStyled = styled.div`
-`;
+  /* border: 1px solid red; */
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    overflow-y: scroll;
+    .basket-card {
+        /* border: 1px solid blue; */
+        margin: 10px 16px;
+        height: 86px;
+        box-sizing: border-box;
+        &:first-child {
+            margin-top: 20px;
+          /* border: 1px solid red; */
+        }
+        &:last-child {
+            margin-bottom: 20px;
+        }
+    }
+`
