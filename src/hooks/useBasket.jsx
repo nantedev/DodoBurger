44f8bd11ceeea3216e8cc20239ffdbcfc021ed90
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-09-19 22:20:54 +0400

    ✨ F11 : Basket  > add product >  create  HandleAddToBasket (when product is ALREADY in  basket)

--- src/hooks/useBasket.jsx
+++ src/hooks/useBasket.jsx
@@ -7,19 +7,29 @@ export const useBasket = () => {
     const [basket, setBasket] = useState(fakeBasket.SMALL)
     //State Handlers
     const handleAddToBasket = (productToAdd) => {
-        //Copie du state
+                //Copie du state
         const basketCopy = deepClone(basket)
         const isProductAlreadyInBasket = findInArray(productToAdd.id, basketCopy) !== undefined
-        //Manip du state
+                //Manip du state
         if(!isProductAlreadyInBasket) {
             const newBasketProduct = {
                 ...productToAdd,
                 quantity: 1,
             }
-        //Update du state
+                //Update du state
         const basketUpdated = [newBasketProduct, ...basketCopy]
         setBasket(basketUpdated)
     }
+        // Product is already in the basket
+            //State
+        const indexOfBasketProductToIncrement = basket.findIndex(
+            (basketProduct) => basketProduct.id === productToAdd.id
+        )
+            //Manip du state
+        basketCopy[indexOfBasketProductToIncrement].quantity += 1
+            //Update state
+        setBasket(basketCopy)
+    
     }
 return { basket, handleAddToBasket }
 }
