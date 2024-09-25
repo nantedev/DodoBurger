7b043d25de26ad2d5d4036db8eac1405a689e8c3
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-09-25 23:15:06 +0400

    🔥 F12: Basket >  useBasket > remove unnecessary basketCopy

--- src/hooks/useBasket.jsx
+++ src/hooks/useBasket.jsx
@@ -38,9 +38,7 @@ export const useBasket = () => {
     }
 
     const handleDeleteBasketProduct = (idBasketProduct) => {
-        
-        const basketCopy = deepClone(basket)
-        const basketUpdated = removeObjectById(idBasketProduct, basketCopy)
+        const basketUpdated = removeObjectById(idBasketProduct, basket)
         setBasket(basketUpdated)
     }
 
