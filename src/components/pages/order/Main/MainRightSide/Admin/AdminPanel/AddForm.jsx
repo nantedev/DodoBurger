5b70237ca5dbbcb0e1f6d7d65d4470d0c4009335
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-09-25 23:54:47 +0400

    🐛 F12: AddForm > fix problem(total replaced by 0) encountered typing  number using comma

--- src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/AddForm.jsx
@@ -4,6 +4,7 @@ import { EMPTY_PRODUCT } from '../../../../../../../enums/product'
 import Form from './Form'
 import SubmitButton from './SubmitButton'
 import { useSuccessMessage } from '../../../../../../../hooks/useSuccessMessage'
+import { replaceFrenchCommaWithDot } from '../../../../../../../utils/maths'
 
 
 export default function AddForm() {
@@ -17,9 +18,14 @@ export default function AddForm() {
     setNewProduct({...newProduct, [name]: value })
   }
 
+
   const handleSubmit = (event) => {
       event.preventDefault()
-      const newProductToAdd = { ...newProduct, id: crypto.randomUUID()} 
+      const newProductToAdd = { 
+        ...newProduct, 
+        id: crypto.randomUUID(),
+        price: replaceFrenchCommaWithDot(newProduct.price),
+      } 
       handleAdd(newProductToAdd)
       setNewProduct(EMPTY_PRODUCT)
       displaySuccessMessage()
