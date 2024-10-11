634507b7e4c1999738a7dd0faf60420e143ddefc
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-10-11 10:49:43 +0400

    ♻️  F14: Basket > Create BasketBody

--- src/components/pages/order/Main/Basket/Basket.jsx
+++ src/components/pages/order/Main/Basket/Basket.jsx
@@ -1,21 +1,17 @@
 import styled from 'styled-components'
 import Total from './Total'
 import Footer from './Footer'
-import EmptyBasket from './EmptyBasket'
-import { useContext } from 'react'
-import OrderContext from '../../../../../context/OrderContext'
-import BasketProducts from "./BasketProducts"
 import { theme } from '../../../../../theme'
-import { isEmpty } from "../../../../../utils/array"
+import BasketBody from './BasketBody/BasketBody'
 
 export default function Basket() {
 
-  const { basket, menu } = useContext(OrderContext)
+  
 
   return (
     <BasketStyled>
         <Total />
-        {isEmpty(basket) ?  <EmptyBasket isLoading={menu === undefined}/> : <BasketProducts />}
+        <BasketBody />
         <Footer />
     </BasketStyled>
   );
--- src/components/pages/order/Main/Basket/BasketBody/BasketProducts.jsx
+++ src/components/pages/order/Main/Basket/BasketBody/BasketProducts.jsx
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
-import BasketCard from './BasketCard';
-import { DEFAULT_IMAGE } from "../../../../../enums/product"
+import BasketCard from '../BasketCard';
+import { DEFAULT_IMAGE } from "../../../../../../enums/product"
 import { useContext } from "react"
-import { findObjectById } from "../../../../../utils/array"
-import OrderContext from '../../../../../context/OrderContext'
-import { checkIfProductIsClicked } from '../MainRightSide/Menu/helper'
+import { findObjectById } from "../../../../../../utils/array"
+import OrderContext from '../../../../../../context/OrderContext'
+import { checkIfProductIsClicked } from '../../MainRightSide/Menu/helper'
 import { TransitionGroup, CSSTransition} from "react-transition-group"
-import { basketAnimation } from '../../../../../theme/animations';
+import { basketAnimation } from '../../../../../../theme/animations';
 
 export default function BasketProducts() {
   const { 
