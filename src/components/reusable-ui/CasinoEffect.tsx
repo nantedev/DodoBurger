732d019498c090c1b1acdb8b04f366924e0f9d2e
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-01-17 11:53:33 +0400

    ♻️ TSE-02 : Add CasinoEffect.tsx

--- src/components/reusable-ui/CasinoEffect.tsx
+++ src/components/reusable-ui/CasinoEffect.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
 import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import styled from 'styled-components'
 
-export default function CasinoEffect({count, className}) {
+type CasinoEffectProps = {
+  count: number,
+  className?: string
+}
+
+export default function CasinoEffect({count, className}: CasinoEffectProps) {
   return (
     <TransitionGroup component={CasinoEffectStyled} className="transition-group">
             <CSSTransition classNames="count-animated" timeout={300} key={count}>
