98faf6c2a547e1cd7142a5acdfd041a47c25862b
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-01-18 21:57:23 +0400

    ♻️ TSE-04: hooks - Add  useMenu.ts

--- src/hooks/useMenu.ts
+++ src/hooks/useMenu.ts
@@ -1,24 +1,31 @@
 import { useState } from "react"
 import { deepClone, removeObjectById, findIndexById } from "../utils/array"
 import { fakeMenu } from "@/fakeData/fakeMenu" 
+import { MenuProduct } from "@/types/Product"
+//@ts-ignore
 import { syncBothMenus } from "../api/product"
 
+
 export const useMenu = () => { 
-    const [menu, setMenu] = useState(undefined)
+    const [menu, setMenu] = useState<MenuProduct[] | undefined>(undefined)
 
     //Comportements (Gestionnaire de state)
-    const handleAdd = (newProduct, username) => {
+    const handleAdd = (newProduct: MenuProduct, username: string) => {
+       if (menu) { 
         //Copie du tableau
         const menuCopy = deepClone(menu)
+
         //Manip copie du tableau
         const menuUpdated = [newProduct, ...menuCopy]
         //Update State
         setMenu(menuUpdated)
         syncBothMenus(username, menuUpdated)
       }
+      }
   
-      const handleDelete = (idOfProductToDelete, username) => {
-        //Copie du state
+      const handleDelete = (idOfProductToDelete: string, username:string) => {
+        if (menu) {
+          //Copie du state
         const menuCopy = deepClone(menu)
         //Manip copie de state
         const menuUpdated = removeObjectById(idOfProductToDelete, menuCopy)
@@ -26,19 +33,20 @@ export const useMenu = () => {
         setMenu(menuUpdated)
         syncBothMenus(username, menuUpdated)
       }
+      }
   
-      const handleEdit = (productBeingEdited, username) => { 
-        //Copie du state
+      const handleEdit = (productBeingEdited: MenuProduct, username: string) => { 
+        if (menu) {//Copie du state
         const menuCopy = deepClone(menu)
         //Manip copie de state
         const indexOfProductToEdit = findIndexById(productBeingEdited.id, menu)
         menuCopy[indexOfProductToEdit] = productBeingEdited
         //Update State
         setMenu(menuCopy)
-        syncBothMenus(username, menuCopy)     
+        syncBothMenus(username, menuCopy) }    
       }
   
-      const resetMenu = (username) => {
+      const resetMenu = (username: string) => {
         setMenu(fakeMenu.LARGE)
         syncBothMenus(username, fakeMenu.LARGE)
     }
