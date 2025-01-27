cdb3ca60b2455aacd2240e9650eb30bd2602fcc7
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-01-27 10:47:16 +0400

    TSE-14: add EditForm.tsx

--- src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/EditForm/EditForm.tsx
+++ src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/EditForm/EditForm.tsx
@@ -4,35 +4,36 @@ import EditInfoMessage from './EditInfoMessage'
 import { useState } from 'react'
 import SavingMessage from './SavingMessage'
 import { useSuccessMessage } from '../../../../../../../../hooks/useSuccessMessage'
+import { useParams } from 'react-router-dom'
 
 export default function EditForm() {
   //State
-  const { username, productSelected, setProductSelected, handleEdit, titleEditRef } = useOrderContext()
-  const [valueOnFocus, setValueOnFocus] = useState()
+  const { productSelected, setProductSelected, handleEdit, titleEditRef } = useOrderContext()
+  const [valueOnFocus, setValueOnFocus] = useState<string>()
   const { isSubmitted: isSaved, displaySuccessMessage } = useSuccessMessage()
-  
+  const {username} = useParams()
+
   //Comportements (event handlers)
-  const handleChange = (event) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const {name, value} = event.target
     const productBeingUpdated = {
       ...productSelected, // a les IDs
       [name]: value,
     }
     setProductSelected(productBeingUpdated) //update le formulaire
-    handleEdit(productBeingUpdated, username) //state handler du menu qui update le menu
+    username && handleEdit(productBeingUpdated, username) //state handler du menu qui update le menu
   }
 
-  const handleOnFocus = (event) => {
+  const handleOnFocus = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const valueOnFocus = event.target.value 
     setValueOnFocus(valueOnFocus)
   }
 
-  const handleOnBlur = (event) => {
+  const handleOnBlur = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const valueOnBlur = event.target.value 
     if (valueOnFocus !== valueOnBlur) {
       displaySuccessMessage()
     }
-    console.log("valueOnBlur :",valueOnBlur)
   }
 
   //Affichage
